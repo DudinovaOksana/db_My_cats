57baf692992e7b8517956816aa731010e0c37fa0
Oksana Dudinova <pogorelayaoksana@gmail.com>
2025-03-06 16:01:09 +0300

    add functions:get type,get type where,get all types

--- src/Conn.java
+++ src/Conn.java
@@ -26,23 +26,50 @@ public class Conn {
     // --------Заполнение таблицы--------
     public void insertType(String type) throws SQLException
     {
-        String insertCat = String.format("INSERT INTO 'types' ('type') VALUES ('%s'); ", type);
+        String insertCat = String.format("INSERT INTO 'types' ('type') VALUES %s ", type);
         statmt.execute(insertCat);
         System.out.println("Таблица заполнена");
     }
     // --------Изменение таблицы--------
     public void update_type(int id, String type) throws SQLException
     {
-        String updateType = String.format("UPDATE types SET type = ('%s') where id =('%d');",type,id);
+        String updateType = String.format("UPDATE types SET type = %s where id =%d",type,id);
         statmt.execute(updateType);
         System.out.println("Запись с id = "+id+" обновлена");
     }
     // --------Удаление записи в таблице--------
     public void delete_type(int id) throws SQLException{
-        String deleteType = String.format("DELETE FROM types WHERE Id = ('%d');",id);
+        String deleteType = String.format("DELETE FROM types WHERE Id = %d",id);
         statmt.execute(deleteType);
         System.out.println("Запись с id = "+id+" удалена");
     }
+    //--------Вывод записи с определенным id--------
+    public String get_type(int id) throws SQLException{
+        String getType = String.format("Select type from types where id=%d",id);
+        statmt.execute(getType);
+        ResultSet resultSet = statmt.getResultSet();
+        return resultSet.getString("type");
+
+    }
+    //--------Вывести на экран котиков,у которых "id < 15" или "type LIKE '%а'"--------
+    public void get_type_where(String where) throws SQLException{
+        String getTypeWhere = String.format("Select type from types where %s",where);
+        statmt.execute(getTypeWhere);
+        ResultSet resultSet = statmt.getResultSet();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("type"));
+        }
+    }
+    // --------Вывод всех пород котиков--------
+    public void get_all_types() throws SQLException{
+        String getAllTypes = String.format("Select type from types");
+        statmt.execute(getAllTypes);
+        ResultSet resultSet = statmt.getResultSet();
+        while (resultSet.next()){
+            System.out.println(resultSet.getString("type"));
+        }
+    }
+
     // -------- Вывод таблицы--------
     public void readDB() throws ClassNotFoundException, SQLException
     {
--- src/Db.java
+++ src/Db.java
@@ -6,13 +6,16 @@ public class Db {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Conn conn = new Conn("jdbc:sqlite:My_cats.s3db");
         conn.createDB();
+        int id = 7;
+          String type = conn.get_type(id);
+        System.out.printf("У id %d тип %s%n", id, type);
 
-        for (String type : Const.TYPES){
-            conn.insertType(type);
-        }
-        conn.update_type(5,"Сибирская двухцветная");
-        conn.delete_type(6);
+        String where = "type LIKE '%а'";
+        conn.get_type_where(where);
+        System.out.println();
+        conn.get_all_types();
         conn.closeDB();
+
 
     }
 }
