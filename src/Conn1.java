1f397022bcef762c29f419a7d04154171e706bdf
Oksana Dudinova <pogorelayaoksana@gmail.com>
2025-03-11 22:50:36 +0300

    add method to insert more cats

--- src/Conn1.java
+++ src/Conn1.java
@@ -29,17 +29,15 @@ public class Conn1 {
     }
     // --------Добавить кошку--------
     public int findCatByType (String type) throws SQLException {
-        //найти typeId
         statmt = connection.createStatement();
         String select = String.format("select id from types where type='%s'",type);
         ResultSet resultSet = statmt.executeQuery(select);
         try  {
             return resultSet.getInt("id");
         } catch (SQLException e) {
-            //Создать typeid, type для новой кошки
             statmt.execute(String.format("insert or ignore into types values (null, '%s')",type));
             statmt.executeQuery(select);
-            return resultSet.getInt("id"); //вернуть typeId НОВЫЙ
+            return resultSet.getInt("id");
         }
     }
     public void insertCat(String name, String type, int age, Double weight) throws SQLException {
@@ -47,6 +45,58 @@ public class Conn1 {
         String insertCat = String.format(Locale.US, "INSERT or ignore INTO cats VALUES (null, '%s', %d, %d, %.1f)", name,typeId,age,weight);
         statmt.execute(insertCat);
         System.out.println("Котик добавлен");
+    }
+    // --------Добавить много котиков--------
+    public void add_more_cats(int n) throws SQLException{
+    for (int i=0; i<n; i++){
+        insertCat(randomCatName(), randomCatType(), randomCatAge(), randomCatWeight());
+    }
+    }
+
+    private String randomCatName(){
+        double seed = Math.random();
+        String[] names = {"Гарфилд","Том","Гудвин","Рокки","Ленивец","Пушок","Спорти","Бегемот","Пират","Гудини","Зорро","Саймон","Альбус","Базилио","Леопольд","Нарцисс","Атос","Каспер","Валлито","Оксфорд","Бисквит","Соня","Клеопатра","Цунами","Забияка","Матильда","Кнопка","Масяня","Царапка","Серсея","Ворсинка","Амели","Наоми","Маркиза","Изольда","Вальс","Несквик","Златан","Баскет","Изюм","Цукат","Мокко","Месси","Кокос","Адидас","Бейлиз","Тайгер","Зефир","Мохи","Валенсия","Баунти","Свити","Текила","Ириска","Карамель","Виски","Кукуруза","Гренка","Фасолька","Льдинка","Китана","Офелия","Дайкири","Брусника","Аватар","Космос","Призрак","Изумруд","Плинтус","Яндекс","Крисмас","Метеор","Оптимус","Смайлик","Цельсий","Краска","Дейзи","Пенка","Веста","Астра","Эйприл","Среда","Бусинка","Гайка","Елка","Золушка","Мята","Радость","Сиам","Оскар","Феликс","Гарри","Байрон","Чарли","Симба","Тао","Абу","Ватсон","Енисей","Измир","Кайзер","Васаби","Байкал","Багира","Айрис","Диана","Мими","Сакура","Индия","Тиффани","Скарлетт","Пикси","Лиззи","Алиса","Лило","Ямайка","Пэрис","Мальта","Аляска"};
+        int randomNameIndex = (int) (seed * names.length);
+        return names[randomNameIndex];
+    }
+
+    private String randomCatType() throws SQLException {
+        double seed = Math.random();
+        int typesCount = getAllTypesCount();
+        int randomNameIndex = (int) (seed * typesCount);
+        return getType(randomNameIndex);
+    }
+
+    public int getAllTypesCount() throws SQLException{
+        statmt = connection.createStatement();
+        String getAllTypes = String.format("SELECT COUNT(id) FROM types;");
+        ResultSet resultSet = statmt.executeQuery(getAllTypes);
+        int res = resultSet.getInt("COUNT(id)");
+        return res-1;
+    }
+
+    private int randomCatAge(){
+        double seed = Math.random();
+        return (int)(seed * 20);
+    }
+
+    private double randomCatWeight(){
+        double seed = Math.random();
+        double randomCatWeight = seed * 10;
+        return randomCatWeight;
+    }
+
+    public String getType(int id) throws SQLException{
+        String getType = String.format("Select type from types where id=%d",id);
+        ResultSet resultSet = statmt.executeQuery(getType);
+        String res;
+        try {
+            res = resultSet.getString("type");
+        } catch (SQLException e) {
+            res = "ERROR";
+        }
+        return res;
+
     }
     // --------Вывод таблицы--------
     public void readDB() throws ClassNotFoundException, SQLException
--- src/Db1.java
+++ src/Db1.java
@@ -4,11 +4,12 @@ import java.sql.SQLException;
 public class Db1 {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Conn1 conn1 = new Conn1("jdbc:sqlite:My_cats.s3db");
-        conn1.createDB();
-        conn1.insertCat("Август", "Гавана",3,5.5);
-        conn1.insertCat("Вася","Ориентальная кошка",1, 4.3);
-        conn1.insertCat("Нафоня","Сококе",2,4.1);
-        conn1.readDB();
+//        conn1.createDB();
+//        conn1.insertCat("Август", "Гавана",3,5.5);
+//        conn1.insertCat("Вася","Ориентальная кошка",1, 4.3);
+//        conn1.insertCat("Нафоня","Сококе",2,4.1);
+        conn1.add_more_cats(5000);
+//        conn1.readDB();
         conn1.closeDB();
     }
 }
